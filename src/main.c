999d262a29c6ddb63362170f83171cdb508ed66e
weeniemount <59868100+weeniemount@users.noreply.github.com>
2024-12-24 16:08:15 +0100

    make some menu bar work

--- src/main.c
+++ src/main.c
@@ -8,77 +8,7 @@
 // Global variables
 HINSTANCE hInst;
 bool buttonPressed[GRID_SIZE * GRID_SIZE] = {false};
-
-// Callback function for the main window
-LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam) {
-	switch (message) {
-		case WM_CREATE: {
-			// Create a grid of buttons
-			for (int row = 0; row < GRID_SIZE; ++row) {
-				for (int col = 0; col < GRID_SIZE; ++col) {
-					HWND hButton = CreateWindow(
-						"BUTTON", 
-						"", // No text
-						WS_VISIBLE | WS_CHILD | BS_PUSHBUTTON, 
-						col * BUTTON_SIZE, 
-						row * BUTTON_SIZE, 
-						BUTTON_SIZE, 
-						BUTTON_SIZE, 
-						hWnd, 
-						(HMENU)(row * GRID_SIZE + col), 
-						hInst, 
-						NULL
-					);
-					if (!hButton) {
-						MessageBox(NULL, "Button creation failed!", "Error", MB_OK);
-					}
-				}
-			}
-			break;
-		}
-		case WM_COMMAND: {
-			// Get the button ID
-			int buttonID = LOWORD(wParam);
-			HWND hButton = (HWND)lParam;
-
- 			buttonPressed[buttonID] = true;
-
-            // Check if all buttons are pressed
-            bool allPressed = true;
-            for (int i = 0; i < GRID_SIZE * GRID_SIZE; ++i) {
-                if (!buttonPressed[i]) {
-                    allPressed = false;
-                    break;
-                }
-            }
-
-            // Display message if all buttons are pressed
-
-			// Disable the button
-			if (hButton) {
-				DestroyWindow(hButton);
-
-				/*EnableWindow(hButton, FALSE);
-				// Change button appearance to "held down"
-				SetWindowLong(hButton, GWL_STYLE, GetWindowLong(hButton, GWL_STYLE) | BS_FLAT);
-				SendMessage(hButton, WM_CTLCOLORBTN, (WPARAM)GetSysColorBrush(COLOR_BTNFACE), 0);
-				InvalidateRect(hButton, NULL, TRUE); // Force redraw*/
-			}
-
-            if (allPressed) {
-                MessageBox(hWnd, "u pressed all ze buttons!", "congrat", MB_OK);
-            }
-			break;
-		}
-		case WM_DESTROY:
-			PostQuitMessage(0);
-			break;
-		default:
-			return DefWindowProc(hWnd, message, wParam, lParam);
-	}
-	return 0;
-}
-
+LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
 
 // WinMain function (entry point)
 int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
@@ -145,3 +75,82 @@ int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLi
 	return (int)msg.wParam;
 }
 
+// Callback function for the main window
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam) {
+	switch (message) {
+		case WM_CREATE: {
+			// Create a grid of buttons
+			for (int row = 0; row < GRID_SIZE; ++row) {
+				for (int col = 0; col < GRID_SIZE; ++col) {
+					HWND hButton = CreateWindow(
+						"BUTTON", 
+						"", // No text
+						WS_VISIBLE | WS_CHILD | BS_PUSHBUTTON, 
+						col * BUTTON_SIZE, 
+						row * BUTTON_SIZE, 
+						BUTTON_SIZE, 
+						BUTTON_SIZE, 
+						hWnd, 
+						(HMENU)(row * GRID_SIZE + col), 
+						hInst, 
+						NULL
+					);
+					if (!hButton) {
+						MessageBox(NULL, "Button creation failed!", "Error", MB_OK);
+					}
+				}
+			}
+			break;
+		}
+		case WM_COMMAND: {
+			// Get the button ID
+			int buttonID = LOWORD(wParam);
+			HWND hButton = (HWND)lParam;
+
+ 			buttonPressed[buttonID] = true;
+
+            // Check if all buttons are pressed
+            bool allPressed = true;
+            for (int i = 0; i < GRID_SIZE * GRID_SIZE; ++i) {
+                if (!buttonPressed[i]) {
+                    allPressed = false;
+                    break;
+                }
+            }
+
+            // Display message if all buttons are pressed
+
+			// Disable the button
+			if (hButton) {
+				DestroyWindow(hButton);
+
+				/*EnableWindow(hButton, FALSE);
+				// Change button appearance to "held down"
+				SetWindowLong(hButton, GWL_STYLE, GetWindowLong(hButton, GWL_STYLE) | BS_FLAT);
+				SendMessage(hButton, WM_CTLCOLORBTN, (WPARAM)GetSysColorBrush(COLOR_BTNFACE), 0);
+				InvalidateRect(hButton, NULL, TRUE); // Force redraw*/
+			}
+
+            if (allPressed) {
+                MessageBox(hWnd, "u pressed all ze buttons!", "congrat", MB_OK);
+            }
+
+			switch (LOWORD(wParam)) {
+				case EXIT:
+                    PostQuitMessage(0);
+                    break;
+                case ABOUT:
+                    MessageBox(hWnd, "just a funny game to click buttons\nmade by boinkwer to push", "abaut button pushher", MB_OK | MB_ICONINFORMATION);
+                    break;
+			}
+
+			break;
+		}
+		case WM_DESTROY:
+			PostQuitMessage(0);
+			break;
+		default:
+			return DefWindowProc(hWnd, message, wParam, lParam);
+	}
+	return 0;
+}
