993fad44aa6ad59035b72a48f2b6af58312bce33
weeniemount <59868100+weeniemount@users.noreply.github.com>
2024-12-24 13:15:16 +0100

    never mind go bck down

--- src/main.c
+++ src/main.c
@@ -6,6 +6,58 @@
 // Global variables
 HINSTANCE hInst;
 
+// Callback function for the main window
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam) {
+	switch (message) {
+		case WM_CREATE: {
+			// Create a grid of buttons
+			for (int row = 0; row < GRID_SIZE; ++row) {
+				for (int col = 0; col < GRID_SIZE; ++col) {
+					HWND hButton = CreateWindow(
+						"BUTTON", 
+						"", // No text
+						WS_VISIBLE | WS_CHILD | BS_PUSHBUTTON, 
+						col * BUTTON_SIZE, 
+						row * BUTTON_SIZE, 
+						BUTTON_SIZE, 
+						BUTTON_SIZE, 
+						hWnd, 
+						(HMENU)(row * GRID_SIZE + col), 
+						hInst, 
+						NULL
+					);
+					if (!hButton) {
+						MessageBox(NULL, "Button creation failed!", "Error", MB_OK);
+					}
+				}
+			}
+			break;
+		}
+		case WM_COMMAND: {
+			// Get the button ID
+			int buttonID = LOWORD(wParam);
+			HWND hButton = (HWND)lParam;
+
+			// Disable the button
+			if (hButton) {
+				EnableWindow(hButton, FALSE);
+				// Change button appearance to "held down"
+				SetWindowLong(hButton, GWL_STYLE, GetWindowLong(hButton, GWL_STYLE) | BS_FLAT);
+				SendMessage(hButton, WM_CTLCOLORBTN, (WPARAM)GetSysColorBrush(COLOR_BTNFACE), 0);
+				InvalidateRect(hButton, NULL, TRUE); // Force redraw
+			}
+			break;
+		}
+		case WM_DESTROY:
+			PostQuitMessage(0);
+			break;
+		default:
+			return DefWindowProc(hWnd, message, wParam, lParam);
+	}
+	return 0;
+}
+
+
 // WinMain function (entry point)
 int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
 	hInst = hInstance;
@@ -49,55 +101,5 @@ int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLi
 	}
 
 	return (int)msg.wParam;
-}
-
-// Callback function for the main window
-LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam) {
-	switch (message) {
-		case WM_CREATE: {
-			// Create a grid of buttons
-			for (int row = 0; row < GRID_SIZE; ++row) {
-				for (int col = 0; col < GRID_SIZE; ++col) {
-					HWND hButton = CreateWindow(
-						"BUTTON", 
-						"", // No text
-						WS_VISIBLE | WS_CHILD | BS_PUSHBUTTON, 
-						col * BUTTON_SIZE, 
-						row * BUTTON_SIZE, 
-						BUTTON_SIZE, 
-						BUTTON_SIZE, 
-						hWnd, 
-						(HMENU)(row * GRID_SIZE + col), 
-						hInst, 
-						NULL
-					);
-					if (!hButton) {
-						MessageBox(NULL, "Button creation failed!", "Error", MB_OK);
-					}
-				}
-			}
-			break;
-		}
-		case WM_COMMAND: {
-			// Get the button ID
-			int buttonID = LOWORD(wParam);
-			HWND hButton = (HWND)lParam;
-
-			// Disable the button
-			if (hButton) {
-				EnableWindow(hButton, FALSE);
-				// Change button appearance to "held down"
-				SetWindowLong(hButton, GWL_STYLE, GetWindowLong(hButton, GWL_STYLE) | BS_FLAT);
-				InvalidateRect(hButton, NULL, TRUE); // Force redraw
-			}
-			break;
-		}
-		case WM_DESTROY:
-			PostQuitMessage(0);
-			break;
-		default:
-			return DefWindowProc(hWnd, message, wParam, lParam);
-	}
-	return 0;
 }
 
